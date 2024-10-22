bed87f1af5abf67e551cbdd2b514174168668b6f
Mohamed Shadhaan <shadhanm@gmail.com>
2024-10-22 17:32:48 +0800

    Push

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,16 @@
+import { Inconsolata } from "next/font/google";
+import "./globals.css";
+
+const font = Inconsolata({
+  subsets: ["latin"],
+  variable: "--font-inconsolata",
+});
+
 export default function RootLayout({ children }) {
   return (
     <html>
       <head />
-      <body>{children}</body>
+      <body className={font.variable}>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null;
+  return (
+    <div className="flex justify-center items-center text-4xl h-screen w-screen font-sans">
+      <span>Hello there</span>
+    </div>
+  );
 }
